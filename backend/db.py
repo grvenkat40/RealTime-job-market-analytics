b22f294eb651d7fe1c34d210a7bc049e7f62ff37
Venkat G R <grvenkat456@gmail.com>
2025-12-15 18:43:23 +0530

    end

--- backend/db.py
+++ backend/db.py
@@ -6,11 +6,12 @@ load_dotenv()
 
 def get_connections():
     return mysql.connector.connect(
-        host = os.getenv("DB_HOST","localhost"),
-        user = os.getenv("DB_USER","root"),
+        host = os.getenv("DB_HOST"),
+        user = os.getenv("DB_USER"),
         password = os.getenv("DB_PASSWORD"),
         database = os.getenv("DB_NAME","job_analytics"),
-        port = int(os.getenv("DB_PORT", 3306))
+        port = int(os.getenv("DB_PORT")),
+        ssl_disabled=False
 
         # host = "localhost",
         # user = "root",
